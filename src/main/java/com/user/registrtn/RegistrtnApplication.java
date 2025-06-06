35f079304b27e57304e6c48f10da8a4157424a8a
yashaswi <yashaswiaddyei@gmail.com>
2025-06-06 12:37:42 +0530

    Initial Spring Boot Config

---
+++ src/main/java/com/user/registrtn/RegistrtnApplication.java
@@ -0,0 +1,13 @@
+package com.user.registrtn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrtnApplication.class, args);
+	}
+
+}
